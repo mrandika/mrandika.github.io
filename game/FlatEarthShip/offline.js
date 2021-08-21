e5de52179491091aacc33a6a20f7a9a85b5de26d
Muhammad Rizki Andika <Mrizkiandika226@gmail.com>
2021-08-21 12:48:32 +0700

    Prepare for revamp

--- game/FlatEarthShip/offline.js
+++
@@ -1,1 +0,0 @@
-{"version":1508078270793,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/shared-0-sheet0.png","images/shared-0-sheet1.png","images/latar-sheet0.png","icons/icon.png","register-sw.js"]}
